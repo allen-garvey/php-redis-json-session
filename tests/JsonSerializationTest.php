66dbd9f02338bfc9c4b7432f48b4ddd88ee0f8b2
Allen Garvey <allengarvey@hotmail.com>
2016-09-17 22:44:43 -0400

    Add tests for serializing and deserializing empty objects

--- tests/JsonSerializationTest.php
+++ tests/JsonSerializationTest.php
@@ -12,12 +12,22 @@ class JsonSerializationTest extends TestCase{
 		$this->assertEquals($serializedArray, $sessionSerializedArray);
 	}
 
+	public function testSessionSerializeEmpty(){
+		$this->assertEquals('', RedisJsonSessionHandler::sessionSerializeArray(null));
+		$this->assertEquals('', RedisJsonSessionHandler::sessionSerializeArray([]));
+	}
+
 	//test session string to array
 	public function unserializeSessionDataToArray(){
 		$testArray = ['test' => 'hello', 'num' => 3, 'array' => [1, 2, 'test'], 'associative array' => ['key1' => 1, 'key2' => 'hello']];
 		$serializedArray = 'test|s:5:"hello";num|i:3;array|a:3:{i:0;i:1;i:1;i:2;i:2;s:4:"test";}associative array|a:2:{s:4:"key1";i:1;s:4:"key2";s:5:"hello";}';
 		$sessionUnserializedArray = RedisJsonSessionHandler::unserializeSessionData($serializedArray);
 		$this->assertEquals($testArray, $sessionUnserializedArray);
+	}
+
+	public function unserializeEmptySessionDataToArray(){
+		$this->assertEquals([], RedisJsonSessionHandler::unserializeSessionData(null));
+		$this->assertEquals([], RedisJsonSessionHandler::unserializeSessionData(''));
 	}
 	
 }
