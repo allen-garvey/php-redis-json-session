d34ae7254af722282af2a2d5e39bbaec38888789
Allen Garvey <allengarvey@hotmail.com>
2016-09-18 22:43:48 -0400

    Add tests to check that session handler correctly parses connection params in constructor

--- tests/InitializationTest.php
+++ tests/InitializationTest.php
@@ -8,6 +8,36 @@ class InitializationTest extends TestCase{
      * @expectedException BadMethodCallException
      */
 	public function testInvalidTCPConnectionString(){
-		$this->redisSessionHandler = new RedisJsonSessionHandler('127.0.0.1');
+		$redisSessionHandler = new RedisJsonSessionHandler('127.0.0.1');
 	}
+
+	/**
+     * @expectedException BadMethodCallException
+     */
+	public function testInvalidConnectionParams(){
+		$redis = RedisJsonSessionHandler::getRedisConnection(['port'=> 6379]);
+	}
+
+	public function testTCPConnectionParamsConstructorParsing(){
+		//test tcp with default port
+		$handler = new RedisJsonSessionHandler('tcp://127.0.0.1');
+		$connectionParams = $handler->getRedisConnectionParams();
+		$expectedParams = ['host' => 'tcp://127.0.0.1', 'port' => 6379];
+		$this->assertEquals($expectedParams, $connectionParams);
+
+		//test tcp with custom port
+		$handler = new RedisJsonSessionHandler('tcp://196.168.1.1', 1234);
+		$connectionParams = $handler->getRedisConnectionParams();
+		$expectedParams = ['host' => 'tcp://196.168.1.1', 'port' => 1234];
+		$this->assertEquals($expectedParams, $connectionParams);
+
+		//test unix socket
+		$handler = new RedisJsonSessionHandler('unix:///var/run/redis/redis.sock');
+		$connectionParams = $handler->getRedisConnectionParams();
+		$expectedParams = ['host' => 'unix:///var/run/redis/redis.sock'];
+		$this->assertEquals($expectedParams, $connectionParams);
+	}
+
+
+
 }
